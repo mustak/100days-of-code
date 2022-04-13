67585a3d137607dca18f06c39bfd0424e64920d0
mustak <mhyaqub@yahoo.com>
2022-04-13 20:08:21 +0100

    add username to tex file as an array.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
 
 const PORT = 3000;
-
 const app = express();
 
 app.use(helmet()); //set security-related HTTP response headers
@@ -15,9 +16,14 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.dir(req.body);
     const { username } = req.body;
 
+    const filePath = path.join(__dirname, 'usernames.json');
+    const fileContent = fs.readFileSync(filePath);
+    const fileData = JSON.parse(fileContent);
+    fileData.push(username);
+
+    fs.writeFileSync(filePath, JSON.stringify(fileData));
     res.send(`Name: ${username} added.`);
 });
 
