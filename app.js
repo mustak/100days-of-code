55b662f0c8031ae1ccefd10961bb1da6a4e90033
mustak <mhyaqub@yahoo.com>
2022-04-17 17:45:46 +0100

    refactor to use router and controller

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+
+const routerUsernames = require('./routes/route-usernames');
 
 const PORT = 3000;
 const app = express();
@@ -9,23 +9,7 @@ const app = express();
 app.use(helmet()); //set security-related HTTP response headers
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send(
-        `<form method='POST'><label>Name: <input type='text' name='username'/></label><button>Submit</button></form>`
-    );
-});
-
-app.post('/', (req, res) => {
-    const { username } = req.body;
-
-    const filePath = path.join(__dirname, 'usernames.json');
-    const fileContent = fs.readFileSync(filePath);
-    const fileData = JSON.parse(fileContent);
-    fileData.push(username);
-
-    fs.writeFileSync(filePath, JSON.stringify(fileData));
-    res.send(`Name: ${username} added.`);
-});
+app.use('/', routerUsernames);
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
